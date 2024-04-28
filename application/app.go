f7f75a3dc4eca02422bb0ec594a452bf6028e126
Chester Riley <criley.dev@gmail.com>
2024-04-28 21:27:33 +0100

    Add redis client

--- application/app.go
+++ application/app.go
@@ -1,19 +1,37 @@
 package application
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
-	"github.com/bvrple/application/routes"
+	"github.com/go-redis/redis/v9"
 )
 
-type App struct { 
-	Router http.Handler
+type App struct {
+	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
-		router: loadRoutes()
+		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
+}
+
+func (a *App) Start(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: a.router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to listen to server: %w", err)
+	}
+
+	return nil
 }
--- application/routes.go
+++ application/routes.go
@@ -1,10 +1,9 @@
 package application
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
+	"github.com/bvrple/orders-api/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -12,6 +11,10 @@ import (
 func loadRoutes() *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 
 	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
@@ -30,18 +33,4 @@ func loadOrderRoutes(router chi.Router) {
 	router.Get("/{id}", orderHandler.GetById)
 	router.Put("/{id}", orderHandler.UpdateById)
 	router.Delete("/{id}", orderHandler.DeleteById)
-}
-
-func (a *App) Start(ctx context.Context) error {
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: a.Router,
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("failed to listen to server: %w", err)
-	}
-
-	return nil
 }
